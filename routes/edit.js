d26544a87ea388796a83f8575f3f65cde914428f
Aokashi <Aokashi.Towa@Gmail.com>
2017-11-05 23:30:45 +0900

    ページの編集を関数に分けた他、内容が空欄の場合は削除できるようにした

--- routes/edit.js
+++ routes/edit.js
@@ -4,6 +4,45 @@ var router = express.Router();
 var fs = require('fs');
 var path = require('path');
 
+/* 汎用関数 */
+function checkPageFile(path) {
+  var result = true;
+  var file = fs.open(path, 'r+', function(fileError, fd) {
+    if (fileError) {
+      result = false;
+    } else {
+      fs.close(fd);
+    }
+  });
+  return result;
+}
+function writePageFile(path, content) {
+  var result = true;
+  console.log(path + 'を書き込みます。');
+  fs.writeFile(path, content, function(fileError) {
+    if (fileError) {
+      console.log('...書き込めませんでした。');
+      result = false;
+    } else {
+      console.log('...書き込みました。');
+    }
+  });
+  return result;
+}
+function deletePageFile(path) {
+  var result = true;
+  console.log(path + 'を削除します。');
+  fs.unlink(path, function(fileError) {
+    if (fileError) {
+      console.log('...削除できませんでした。');
+      result = false;
+    } else {
+      console.log('...削除しました。');
+    }
+  });
+  return result;
+}
+
 /* 編集画面 */
 router.get('/*', function(req, res, next) {
   var fileURL = req.url;
@@ -26,23 +65,29 @@ router.get('/*', function(req, res, next) {
 
 /* 編集を反映 */
 router.post('/', function(req, res, next) {
-  if (!req.body.url || !req.body.content) {
+  if (req.body.url == null || req.body.content == null) {
     var err = new Error('Wrong POST Method!');
     err.status = 403;
     next(err);
   }
-  console.log(path.join(path.resolve('content'), req.body.url + '.md') + 'を書き込みます。');
-  fs.writeFile(path.join(path.resolve('content'), req.body.url + '.md'), req.body.content, function(fileError) {
-    if (fileError) {
-      console.log('...書き込めませんでした。');
+  var filePath = path.join(path.resolve('content'), req.body.url + '.md');
+  if(!req.body.content && checkPageFile(filePath)) { // 2つ目の条件はファイルが存在するか確認するため
+    if (deletePageFile(filePath)) {
+      res.redirect(req.body.url);
+    } else {
+      var err = new Error('Cannot Delete File!');
+      err.status = 403;
+      next(err);
+    }
+  } else {
+    if (writePageFile(filePath, req.body.content)) {
+      res.redirect(req.body.url);
+    } else {
       var err = new Error('Cannot Write File!');
       err.status = 403;
       next(err);
-    } else {
-      console.log('...書き込みました。');
-      res.redirect(req.body.url);
     }
-  });
+  }
 });
 
 module.exports = router;
