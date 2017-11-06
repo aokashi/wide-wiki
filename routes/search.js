61f6e47837af66cbaaf0ab36178f7f4d5ca098d1
Aokashi <Aokashi.Towa@Gmail.com>
2017-11-06 22:14:15 +0900

    検索機能の見た目だけを実装した

--- routes/search.js
+++ routes/search.js
@@ -4,9 +4,11 @@ var config = require('../config');
 
 /* 検索画面 */
 router.get('/', function(req, res, next) {
+  console.log(req.query);
   res.render('view',{
     title: 'ページの検索',
     mode: 'search',
+    hasQuery: false,
     siteConfig: config
   });
 });
